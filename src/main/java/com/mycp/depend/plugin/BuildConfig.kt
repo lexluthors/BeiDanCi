25ce9e3465d1c9c2cfbab5773dce174b1cdccfd6
liulei <823581722@qq.com>
2022-04-05 20:57:24 +0800

    更新使用第三方开源广告sdk，封装自己的sdk

--- src/main/java/com/mycp/depend/plugin/BuildConfig.kt
+++ src/main/java/com/mycp/depend/plugin/BuildConfig.kt
@@ -9,7 +9,7 @@ object BuildConfig {
     const val compileSdkVersion = 30
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 19
-    const val targetSdkVersion = 29
+    const val targetSdkVersion = 30
     const val versionCode = 10000
     const val versionName = "1.0.0"
 
